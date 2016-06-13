208d56463073014c13975f6a67f7250a9e28c040
mathcsboston <zhenyu@wireless1x-168-122-14-217.bu.edu>
2016-06-12 21:48:27 -0400

    modefy

--- TEST.py
+++ TEST.py
@@ -1,3 +1,8 @@
+import tensorflow as tf
+W = tf.Variable(tf.zeros([784, 10]))
+print W
+
+'''
 print 'Hello'
 def fibo(a,b):
 	if a==0 or b==0:
@@ -12,3 +17,4 @@ print 'As you wish, second version'
 d=fibo(5,10)
 
 print d
+'''
