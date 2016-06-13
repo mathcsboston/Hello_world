8f34e2080109eccca2d50eef25f36429b66b3d6a
mathcsboston <zhenyu@wireless1x-168-122-14-217.bu.edu>
2016-06-12 21:32:24 -0400

    new change

--- TEST.py
+++ TEST.py
@@ -1,6 +1,14 @@
 print 'Hello'
 def fibo(a,b):
-	if a or b:
+	if a==0 or b==0:
+		print a,b
 		return a+b
 	else:
 		return fibo(a-1,b-1)
+
+print 'As you wish, second version'
+
+
+d=fibo(5,10)
+
+print d
